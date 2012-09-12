7d2c8d30cad2ff2a4dfddecb950876f5a8576e27
mallowlabs <mallowlabs@gmail.com>
2012-09-12 10:01:42 +0900

    Fixed: url

--- init.rb
+++ init.rb
@@ -13,6 +13,6 @@ Redmine::Plugin.register :redmine_absolute_dates do
   author 'suer'
   description 'Display absolute create or update dates '
   version '0.0.1'
-  url 'http://d.hatena.ne.jp/suer'
+  url 'https://github.com/suer/redmine_absolute_dates'
   author_url 'http://d.hatena.ne.jp/suer'
 end
