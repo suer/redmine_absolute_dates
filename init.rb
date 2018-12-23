5e1a527519032ac841c5d1ad6e748a842e45f2e9
Ryo SUETSUGU <suetsugu.r@gmail.com>
2018-12-23 21:51:17 +0900

    Bumped: Ver.0.0.4

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_absolute_dates do
   name 'Redmine Absolute Dates plugin'
   author 'suer'
   description 'Display absolute create or update dates '
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/suer/redmine_absolute_dates'
   author_url 'http://d.hatena.ne.jp/suer'
 end
